e5357869c7be6054a2d38e88260adf866b7b9fcd
DANTE\nguye <nguyenminhtam2288@gmail.com>
2025-03-04 12:32:42 +0700

    Done BreezyDrive.Conversations Entities

---
+++ BreezyDrive.Conversations/Entities/Conversation.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BreezyDrive.Conversations.Entities
+{
+    [Table("Conversation")]
+    public class Conversation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public Guid UserId1 { get; set; }
+        public Guid UserId2 { get; set; }
+        public string LastMessage { get; set; }
+        public bool IsOpen { get; set; }
+        public Guid? CloseAccountId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual Users UserId1 { get; set; }
+        [ForeignKey("UserId")]
+        public virtual Users UserId2 { get; set; }
+
+    }
+}
