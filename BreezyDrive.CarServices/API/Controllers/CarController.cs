f6251f247d10e38a2750967f341f03fb9a8d5c88
KasumiiUwU <p.khoa62@gmail.com>
2025-03-10 15:49:15 +0700

    add controller

---
+++ BreezyDrive.CarServices/API/Controllers/CarController.cs
@@ -0,0 +1,20 @@
+﻿using CoreApiResponse;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BreezyDrive.CarServices.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CarController : BaseController
+{
+    
+    [HttpGet("ahihi")]
+    public IActionResult ahihi()
+    {
+        var result = "ahjiihih";
+        return CustomResult("AHihi");
+    }
+    
+    
+    
+}
--- BreezyDrive.CarServices/Program.cs
+++ BreezyDrive.CarServices/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,6 +37,7 @@ app.MapGet("/weatherforecast", () =>
     })
     .WithName("GetWeatherForecast")
     .WithOpenApi();
+app.MapControllers();
 
 app.Run();
 
