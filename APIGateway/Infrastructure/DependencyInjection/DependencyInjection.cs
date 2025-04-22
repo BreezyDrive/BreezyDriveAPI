632de583e376f737d84c34f86a50f4d2dec784fc
KasumiiUwU <p.khoa62@gmail.com>
2025-04-22 11:37:56 +0700

    fix routing api gateway

--- APIGateway/Infrastructure/DependencyInjection/DependencyInjection.cs
+++ APIGateway/Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -7,14 +7,15 @@ namespace APIGateway.Infrastructure.DependencyInjection;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApiGatewayServices(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddApiGatewayServices(this IServiceCollection services,
+        IConfiguration configuration)
     {
         services.AddReverseProxy(configuration);
         services.AddSwaggerDocumentation();
         services.AddCoreServices();
         return services;
     }
-    
+
     private static IServiceCollection AddCoreServices(this IServiceCollection services)
     {
         //services.AddControllers();
@@ -35,7 +36,7 @@ public static class DependencyInjection
 
         return services;
     }
-    
+
     private static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
     {
         services.AddSwaggerGen();
@@ -49,210 +50,187 @@ public static class DependencyInjection
 
 
         return services;
-    
     }
-    
+
+
+    // private static RouteConfig[] GetRoutes()
+    // {
+    //     return
+    //     [
+    //         //users-route
+    //         new RouteConfig
+    //         {
+    //             RouteId = "users-route",
+    //             ClusterId = "users-cluster",
+    //             Match = new RouteMatch { Path = "users-api/{**catch-all}" },
+    //             Transforms =
+    //             [
+    //                 new Dictionary<string, string> { ["PathPattern"] = "{**catch-all}" }
+    //             ]
+    //         },
+    //
+    //         //Cars-route
+    //         new RouteConfig
+    //         {
+    //             RouteId = "cars-route",
+    //             ClusterId = "cars-cluster",
+    //             Match = new RouteMatch { Path = "cars-api/{**catch-all}" },
+    //             Transforms =
+    //             [
+    //                 new Dictionary<string, string> { ["PathPattern"] = "{**catch-all}" }
+    //             ]
+    //         },
+    //
+    //         //conversations-route
+    //         new RouteConfig
+    //         {
+    //             RouteId = "conversations-route",
+    //             ClusterId = "conversations-cluster",
+    //             Match = new RouteMatch { Path = "conversations-api/{**catch-all}" },
+    //             Transforms =
+    //             [
+    //                 new Dictionary<string, string> { ["PathPattern"] = "{**catch-all}" }
+    //             ]
+    //         },
+    //         //authentication-route
+    //         new RouteConfig
+    //         {
+    //             RouteId = "authentication-route",
+    //             ClusterId = "authentication-cluster",
+    //             Match = new RouteMatch { Path = "authentication-api/{**catch-all}" },
+    //             Transforms =
+    //             [
+    //                 new Dictionary<string, string> { ["PathPattern"] = "{**catch-all}" }
+    //             ]
+    //         }
+    //     ];
+    // }
+    //
+    // private static ClusterConfig[] GetClusters()
+    // {
+    //     return
+    //     [
+    //         new ClusterConfig
+    //         {
+    //             ClusterId = "users-cluster",
+    //             Destinations = new Dictionary<string, DestinationConfig>
+    //             {
+    //                 { "destination1", new DestinationConfig { Address = "http://localhost:8180" } }
+    //             },
+    //         },
+    //         new ClusterConfig
+    //         {
+    //             ClusterId = "cars-cluster",
+    //             Destinations = new Dictionary<string, DestinationConfig>
+    //             {
+    //                 { "destination1", new DestinationConfig { Address = "http://localhost:8280" } }
+    //             },
+    //         },
+    //         new ClusterConfig
+    //         {
+    //             ClusterId = "conversations-cluster",
+    //             Destinations = new Dictionary<string, DestinationConfig>
+    //             {
+    //                 { "destination1", new DestinationConfig { Address = "http://localhost:8380" } }
+    //             },
+    //         },
+    //
+    //         new ClusterConfig
+    //         {
+    //             ClusterId = "authentication-cluster",
+    //             Destinations = new Dictionary<string, DestinationConfig>
+    //             {
+    //                 { "destination1", new DestinationConfig { Address = "http://localhost:8480" } }
+    //             },
+    //         }
+    //     ];
+    // }
     
     private static RouteConfig[] GetRoutes()
     {
-        return
-        [
-            //users-route
-            new RouteConfig
-            {
-            RouteId = "users-route",
-            ClusterId = "users-cluster",
-            Match = new RouteMatch { Path = "users-api/{**catch-all}" },
-            Transforms =
+        var routesInfo = new List<(string RouteId, string ClusterId, string PrefixPath)>
+        {
+            ("users-route", "users-cluster", "users-api"),
+            ("cars-route", "cars-cluster", "cars-api"),
+            ("conversations-route", "conversations-cluster", "conversations-api"),
+            ("authentication-route", "authentication-cluster", "authentication-api")
+        };
+
+        return routesInfo.Select(route => new RouteConfig
+        {
+            RouteId = route.RouteId,
+            ClusterId = route.ClusterId,
+            Match = new RouteMatch { Path = $"{route.PrefixPath}/{{**catch-all}}" },
+            Transforms = 
             [
                 new Dictionary<string, string> { ["PathPattern"] = "{**catch-all}" }
             ]
-            },
-            
-            //Cars-route
-            new RouteConfig
-            {
-                RouteId = "cars-route",
-                ClusterId = "cars-cluster",
-                Match = new RouteMatch { Path = "cars-api/{**catch-all}" },
-                Transforms =
-                [
-                    new Dictionary<string, string> { ["PathPattern"] = "{**catch-all}" }
-                ]
-            },
-            
-            //conversations-route
-            new RouteConfig
-            {
-                RouteId = "conversations-route",
-                ClusterId = "conversations-cluster",
-                Match = new RouteMatch { Path = "conversations-api/{**catch-all}" },
-                Transforms =
-                [
-                    new Dictionary<string, string> { ["PathPattern"] = "{**catch-all}" }
-                ]
-            },
-            //authentication-route
-            new RouteConfig
-            {
-                RouteId = "authentication-route",
-                ClusterId = "authentication-cluster",
-                Match = new RouteMatch { Path = "authentication-api/{**catch-all}" },
-                Transforms =
-                [
-                    new Dictionary<string, string> { ["PathPattern"] = "{**catch-all}" }
-                ]
-            }
-        ];
+        }).ToArray();
     }
 
     private static ClusterConfig[] GetClusters()
     {
-        return
-        [
-            new ClusterConfig
-            {
-                ClusterId = "users-cluster",
-                Destinations = new Dictionary<string, DestinationConfig>
-                {
-                    { "destination1", new DestinationConfig { Address = "http://localhost:8180" } }
-                }, 
+        var clustersInfo = new List<(string ClusterId, string Address)>
+        {
+            ("users-cluster", "http://localhost:8180"),
+            ("cars-cluster", "http://localhost:8280"),
+            ("conversations-cluster", "http://localhost:8380"),
+            ("authentication-cluster", "http://localhost:8480")
+        };
 
-            },
-            new ClusterConfig
+        return clustersInfo.Select(cluster => new ClusterConfig
+        {
+            ClusterId = cluster.ClusterId,
+            Destinations = new Dictionary<string, DestinationConfig>
             {
-                ClusterId = "cars-cluster",
-                Destinations = new Dictionary<string, DestinationConfig>
-                {
-                    { "destination1", new DestinationConfig { Address = "http://localhost:8280" } }
-                },
-
-            },
-            new ClusterConfig
-            {
-                ClusterId = "conversations-cluster",
-                Destinations = new Dictionary<string, DestinationConfig>
-                {
-                    { "destination1", new DestinationConfig { Address = "http://localhost:8380" } }
-                },
-
-            },
-            
-            new ClusterConfig
-            {
-                ClusterId = "authentication-cluster",
-                Destinations = new Dictionary<string, DestinationConfig>
-                {
-                    { "destination1", new DestinationConfig { Address = "http://localhost:8480" } }
-                },
-
+                { "destination1", new DestinationConfig { Address = cluster.Address } }
             }
-        ];
+        }).ToArray();
     }
 
     
+    
     private static ReverseProxyDocumentFilterConfig GetSwaggerConfig()
-{
-    return new ReverseProxyDocumentFilterConfig
     {
-        Routes = GetRoutes().ToDictionary(c => c.RouteId, c => c),
-        Clusters = new Dictionary<string, ReverseProxyDocumentFilterConfig.Cluster>
+        var clustersInfo = new List<(string ClusterName, string Address, string PrefixPath)>
         {
-            {
-                "users-cluster", new ReverseProxyDocumentFilterConfig.Cluster
-                {
-                    Destinations = new Dictionary<string, ReverseProxyDocumentFilterConfig.Cluster.Destination>
-                    {
-                        {
-                            "destination1", new ReverseProxyDocumentFilterConfig.Cluster.Destination
-                            {
-                                Address = "http://localhost:8180",
-                                Swaggers =
-                                [
-                                    new ReverseProxyDocumentFilterConfig.Cluster.Destination.Swagger
-                                    {
-                                        PrefixPath = "/users-api",
-                                        Paths = ["/swagger/v1/swagger.json"]
-                                    }
-                                ]
-                            }
-                        }
-                    }
-                }
-            },
-            {
-                "cars-cluster", new ReverseProxyDocumentFilterConfig.Cluster
-                {
-                    Destinations = new Dictionary<string, ReverseProxyDocumentFilterConfig.Cluster.Destination>
-                    {
-                        {
-                            "destination1", new ReverseProxyDocumentFilterConfig.Cluster.Destination
-                            {
-                                Address = "http://localhost:8280",
-                                Swaggers =
-                                [
-                                    new ReverseProxyDocumentFilterConfig.Cluster.Destination.Swagger
-                                    {
-                                        PrefixPath = "/cars-api",
-                                        Paths = ["/swagger/v1/swagger.json"]
-                                    }
-                                ]
-                            }
-                        }
-                    }
-                }
-            },
-            {
-                "conversations-cluster", new ReverseProxyDocumentFilterConfig.Cluster
-                {
-                    Destinations = new Dictionary<string, ReverseProxyDocumentFilterConfig.Cluster.Destination>
-                    {
-                        {
-                            "destination1", new ReverseProxyDocumentFilterConfig.Cluster.Destination
-                            {
-                                Address = "http://localhost:8380",
-                                Swaggers =
-                                [
-                                    new ReverseProxyDocumentFilterConfig.Cluster.Destination.Swagger
-                                    {
-                                        PrefixPath = "/conversations-api",
-                                        Paths = ["/swagger/v1/swagger.json"]
-                                    }
-                                ]
-                            }
-                        }
-                    }
-                }
-            },
-            {
-                "authentication-cluster", new ReverseProxyDocumentFilterConfig.Cluster
-                {
-                    Destinations = new Dictionary<string, ReverseProxyDocumentFilterConfig.Cluster.Destination>
-                    {
-                        {
-                            "destination1", new ReverseProxyDocumentFilterConfig.Cluster.Destination
-                            {
-                                Address = "http://localhost:8480",
-                                Swaggers =
-                                [
-                                    new ReverseProxyDocumentFilterConfig.Cluster.Destination.Swagger
-                                    {
-                                        PrefixPath = "/authentication-api",
-                                        Paths = ["/swagger/v1/swagger.json"]
-                                    }
-                                ]
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    };
-}
+            ("users-cluster", "http://localhost:8180", "/users-api"),
+            ("cars-cluster", "http://localhost:8280", "/cars-api"),
+            ("conversations-cluster", "http://localhost:8380", "/conversations-api"),
+            ("authentication-cluster", "http://localhost:8480", "/authentication-api"),
+        };
 
-    
-    
-    
+        return new ReverseProxyDocumentFilterConfig
+        {
+            Routes = GetRoutes().ToDictionary(c => c.RouteId, c => c),
+            Clusters = BuildClusters(clustersInfo)
+        };
+    }
 
-    
+    private static Dictionary<string, ReverseProxyDocumentFilterConfig.Cluster> BuildClusters(
+        List<(string ClusterName, string Address, string PrefixPath)> clustersInfo)
+    {
+        return clustersInfo.ToDictionary(
+            cluster => cluster.ClusterName,
+            cluster => new ReverseProxyDocumentFilterConfig.Cluster
+            {
+                Destinations = new Dictionary<string, ReverseProxyDocumentFilterConfig.Cluster.Destination>
+                {
+                    {
+                        "destination1", new ReverseProxyDocumentFilterConfig.Cluster.Destination
+                        {
+                            Address = cluster.Address,
+                            Swaggers = 
+                            [
+                                new ReverseProxyDocumentFilterConfig.Cluster.Destination.Swagger
+                                {
+                                    PrefixPath = cluster.PrefixPath,
+                                    Paths = ["/swagger/v1/swagger.json"]
+                                }
+                            ]
+                        }
+                    }
+                }
+            });
+    }
 }
