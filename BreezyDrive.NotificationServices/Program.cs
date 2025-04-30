ac3e384ce939f90f2d8b3ad4470b9d0ed7574860
KasumiiUwU <p.khoa62@gmail.com>
2025-04-30 10:39:00 +0700

    gom các connection string của CarService, UserService, NotificationService  về common service (shared.appsetting.json)

--- BreezyDrive.NotificationServices/Program.cs
+++ BreezyDrive.NotificationServices/Program.cs
@@ -5,6 +5,11 @@ using BreezyDrive.NotificationServices.Infrastructure.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .AddJsonFile(Path.GetFullPath(Path.Combine(@"../BreezyDrive.CommonService/shared.appsettings.json")), optional: true, reloadOnChange: true)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
 builder.Services.AddNotificationServices(builder.Configuration);
 
 var app = builder.Build();
