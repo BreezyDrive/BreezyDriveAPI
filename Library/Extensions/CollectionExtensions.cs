7503b00b1f69b12b11fff7c6eae0785b8b6410ad
KasumiiUwU <p.khoa62@gmail.com>
2026-05-23 14:54:32 +0700

    migrate to dotnet 10.0

---
+++ Library/Extensions/CollectionExtensions.cs
@@ -0,0 +1,11 @@
+namespace System.Collections.Generic;
+
+using System.Linq;
+
+public static class CollectionExtensions
+{
+    public static bool IsNullOrEmpty<T>(this IEnumerable<T>? source)
+    {
+        return source == null || !source.Any();
+    }
+}
